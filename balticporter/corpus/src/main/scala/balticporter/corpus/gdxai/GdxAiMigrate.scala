4549821cef35ec2d7eecc95bce09804fa277e975
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-09-03 01:17:26 +0200

    retarget dependents: body-sub ObjectMap construction uses apply (private ctor)
    lls ObjectMap constructor is PRIVATE. The body substitution for
    findMetadata used `new lowlevel.util.ObjectMap[...]()` which cannot
    access the private constructor. Changed to `lowlevel.util.ObjectMap[...]()`
    which resolves to the companion's apply factory (Scala 3 universal apply).
    
    ai 11 -> 10 (one E173 at findMetadata line 353 closed).

--- balticporter/corpus/src/main/scala/balticporter/corpus/gdxai/GdxAiMigrate.scala
+++ balticporter/corpus/src/main/scala/balticporter/corpus/gdxai/GdxAiMigrate.scala
@@ -384,7 +384,7 @@ object GdxAiPolicy:
               |      val meta: sge.ai.btree.utils.TaskRegistry.Meta = sge.ai.btree.utils.TaskRegistry.metaOf(clazz)
               |      if (meta != null) {
               |        val taskAttributes: lowlevel.util.ObjectMap[java.lang.String, sge.ai.btree.utils.BehaviorTreeParser.DefaultBehaviorTreeReader.AttrInfo] =
-              |          new lowlevel.util.ObjectMap[java.lang.String, sge.ai.btree.utils.BehaviorTreeParser.DefaultBehaviorTreeReader.AttrInfo]()
+              |          lowlevel.util.ObjectMap[java.lang.String, sge.ai.btree.utils.BehaviorTreeParser.DefaultBehaviorTreeReader.AttrInfo]()
               |        val attrs: scala.Array[sge.ai.btree.utils.TaskRegistry.Attr] = meta.attributes
               |        var i: scala.Int = 0
               |        while (i < attrs.length) {
